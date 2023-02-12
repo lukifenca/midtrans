bfd4ea34af2f64f3d3f97ce846baefd613572840
Lukito Raharjo <56954345+lukifenca@users.noreply.github.com>
2023-02-12 14:25:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,52 @@
 <?php
 
-// replace with your actual server key
-$serverKey = 'SB-Mid-server-gluxBxY-uVxizDzkxad_5JCX';
+$serverKey = "SB-Mid-server-gluxBxY-uVxizDzkxad_5JCX";
+$apiUrl = "https://app.sandbox.midtrans.com/snap/v2/transactions";
 
-// replace with your transaction data
-$data = [
-    "transaction_details" => [
-        "order_id" => "order-123",
-        "gross_amount" => 200000
+$orderId = "ORDER-ID";
+$grossAmount = "100000";
+
+$request = [
+    'transaction_details' => [
+        'order_id' => $orderId,
+        'gross_amount' => $grossAmount
     ],
-    "customer_details" => [
-        "first_name" => "Andi",
-        "last_name" => "Susanto",
-        "email" => "andi.susanto@example.com",
-        "phone" => "081234567890"
-    ],
-    "item_details" => [
+    'item_details' => [
         [
-            "id" => "item-1",
-            "price" => 100000,
-            "quantity" => 2,
-            "name" => "Product 1"
+            'id' => "Item 1",
+            'price' => 100000,
+            'quantity' => 1,
+            'name' => "Item 1"
         ]
     ]
 ];
 
- $apiUrl = "https://app.sandbox.midtrans.com/snap/v2/transactions";
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $apiUrl,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode($request),
+    CURLOPT_HTTPHEADER => [
+        "accept: application/json",
+        "content-type: application/json",
+        "Authorization: Basic " . base64_encode($serverKey . ":")
+    ],
+));
 
-// Set request header
-$header = [
-    "Accept: application/json",
-    "Content-Type: application/json",
-    "Authorization: Basic " . base64_encode($serverKey . ":")
-];
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-// Send transaction data to Midtrans API
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $apiUrl);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_close($curl);
 
-$response = curl_exec($ch);
-curl_close($ch);
-
-// Parse response
-$result = json_decode($response, true);
-
-// Display transaction result
-echo "Transaction status: " . $result["transaction_status"] . "\n";
-echo "Transaction ID: " . $result["transaction_id"] . "\n";
-echo "Transaction Time: " . $result["transaction_time"] . "\n";
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
 
 ?>
